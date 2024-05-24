4819def8118702b0f2fca2a5c30252b94101502f
kentoshiohara <a.mode.night.show@gmail.com>
2024-05-24 10:10:07 +0900

    select得点表追加

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#winningScore');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = parseInt(winningScoreSelect.value);
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
         p1Score++;
         p1Display.textContent = p1Score;
-        if (p1Score == winningScore) {
+        if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
     }
-})
+});
+
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score++;
         p2Display.textContent = p2Score;
-        if (p2Score == winningScore) {
+        if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
     }
+});
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
 })
 
-reset.addEventListener('click', function() {
+resetButton.addEventListener('click', function() {
+    reset();
+});
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
-})
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
